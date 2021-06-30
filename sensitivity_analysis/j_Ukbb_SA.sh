cc48477ecaa55a7a293027b266223cca58f13cc0
Ciarrah <50405084+Ciarrah@users.noreply.github.com>
2021-06-30 13:01:48 +0100

    Add files via upload
    Shell scripts to run varying prior probability and varying prior sigma sensitivity analyses

---
+++ sensitivity_analysis/j_Ukbb_SA.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=1:mem=40gb
+#PBS -l walltime=05:00:00
+#PBS -o output.file
+#---------------------------------------------
+date
+
+module add lang/r/4.0.3-bioconductor-gcc
+
+cd $PBS_O_WORKDIR
+
+Rscript VZ_summary_mvMR_BF_function.R
+
+Rscript VZ_summary_mvMR_SSS_function.R
+
+Rscript UKBB_SA.R
+
+date
